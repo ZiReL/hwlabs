ba0e13c7e3bc32ccecc14ec0f1e3f7550653e00b
Aleksandr Svistunov <asvistunov@sfedu.ru>
2023-10-08 16:56:17 +0300

    Приветствие на 3-х разных языках

--- hello.pas
+++ hello.pas
@@ -1,3 +1,13 @@
 ﻿begin
-  Println('Привет, мир!');
+  Println('Language:');
+  Println('1) Русский');
+  Println('2) English');
+  Println('3) Spanish');
+  var n := ReadInteger('Select number:');
+  Assert((n > 0) and (n < 4));
+  case n of
+    1: Println('Привет, мир!');
+    2: Println('Hello, world!');
+    3: Println('Hola mundo!');
+  end;
 end.
